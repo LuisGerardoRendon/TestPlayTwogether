d7caff9ff88faeb8889130ca4e4277ec0fa78e84
Efrain Arenas <efrain_escuela@outlook.com>
2021-12-10 22:01:03 -0600

    Pruebas SignUp WIP

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -1,3 +1,4 @@
 public class Config {
     public static String loginRute = "http://localhost/PlayTwogetherFrontend/";
+    public static String signupRute = "http://localhost/PlayTwogetherFrontend/view/signup.html";
 }
